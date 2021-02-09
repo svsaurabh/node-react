e69d5cde9b8595546b60f3eca73933255bf5db1c
Saurabh Vishwakarma <svsaurabh3232@gmail.com>
2021-02-10 02:38:36 +0530

    Added PORT

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
 const app = express();
+
+const port = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
     res.send("Node Homepage")
 });
 
 app.listen(5000,()=>{
-    console.log(`App is running at 5000`);
+    console.log(`App is running at ${port}`);
 });
