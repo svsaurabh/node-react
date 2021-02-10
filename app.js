ef025e94bafe0976474a410cfa22cc087fff709e
Saurabh Vishwakarma <svsaurabh3232@gmail.com>
2021-02-10 19:44:36 +0530

    Included react

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+const path = require("path");
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-    res.send("Node Homepage")
+app.get('/api',(req,res)=>{
+    res.send("Node API")
 });
+
+app.use(express.static(path.join(__dirname, "build")));
+//app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+
 
 app.listen(port,()=>{
     console.log(`App is running at ${port}`);
